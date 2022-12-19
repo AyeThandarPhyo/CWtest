dd23b4401d61d912a16dfa880bc06c0b0f1f0c5c
Aye Thandar Phyo <40524297@live.napier.ac.uk>
2022-12-19 11:22:19 +0630

    Added City class.

---
+++ src/main/java/com/napier/sem/City.java
@@ -0,0 +1,15 @@
+package com.napier.sem;
+
+/**
+ * Represents an employee
+ */
+public class City
+{
+    public int ID;
+
+    public String Name;
+
+    public String District;
+
+    public int Population;
+}
